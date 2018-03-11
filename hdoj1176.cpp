8dcf9a4cad173a697dddcfd50bf9cc0287fb419a
zsy <djytyang@gmail.com>
2018-03-11 17:03:12 +0800

    hdoj 1176

--- hdoj1176.cpp
+++ hdoj1176.cpp
@@ -1,38 +1,32 @@
+
 #include<cstdio>
 #include<cmath>
 #include<iostream>
 #include<algorithm>
 #include<cstring>
-#include<string.h>
-#include<stdlib.h>
+#include<cstdlib>
 #include<queue>
 
 using namespace std;
+int dp[100005][15];
 
-
-int main() {
-    int a;
-    int dp[100005][15];
+int main(void) {
     int time, pos;
     int n;
 
     while (scanf("%d", &n) == 1 && n) {
-        int endTime = 0;
         memset(dp, 0, sizeof(dp));
+        int end_Time = 0;
         for (int i = 1; i <= n; i++) {
             scanf("%d %d", &pos, &time);
             dp[time][pos]++;
-            if (time > endTime) {
-                endTime = time;
+            if (time > end_Time) {
+                end_Time = time;
             }
         }
-        for (int i = endTime - 1; i >= 0; i--) {
+        for (int i = end_Time - 1; i >= 0; i--) {
             dp[i][0] += max(dp[i + 1][1], dp[i + 1][0]);
             for (int j = 1; j <= 10; j++) {
-//                int temp = 0;
-//                temp = max(dp[i + 1][j], dp[i + 1][j + 1]);
-//                temp = max(dp[i + 1][j - 1], temp);
-//                dp[i][j] += temp;
                 dp[i][j] += max(dp[i + 1][j],
                                 max(dp[i + 1][j + 1],
                                     dp[i + 1][j - 1]));
