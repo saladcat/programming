47106c62bc329f5bfd1f8a59c492b9db69820594
zhushiyang <a215900031@gmail.com>
2019-06-03 14:48:58 +0800

    次短路径

--- short_path/poj3255.cpp
+++ short_path/poj3255.cpp
@@ -5,41 +5,45 @@
 #include <algorithm>
 #include <queue>
 #include <memory>
+#include <vector>
 
-#define MAX 5010
+#define MAX 5005
 #define INF 0x3f3f3f3f
 #define CostDest pair<int,int>
 using namespace std;
-int N, R;//N节点数量，R边的数量
-int maze[MAX][MAX];//距离矩阵
-int dist1[MAX]; //最短
-int dist2[MAX];//次短
-int vis[MAX];// 不能用vis剪枝，因为vis限制了求次短路径
-typedef struct Edge {
-    int src;
+struct Edge {
     int dest;
     int weight;
-
-    bool operator<(const Edge a) const {
-        return weight < a.weight;
-    }
-} Edge;
+};
+int N, R;//N节点数量，R边的数量
+int dist1[MAX]; //最短
+int dist2[MAX];//次短
+vector<Edge> maze[MAX];
 
 void readData() {
     memset(dist1, INF, sizeof(dist1));
-    memset(maze, INF, sizeof(maze));
     memset(dist2, INF, sizeof(dist2));
-    memset(vis, 0, sizeof(vis));
     cin >> N >> R;
-    int x, y;
+    int x, y, cost;
     for (int i = 0; i < R; i++) {
-        cin >> x >> y;
-        cin >> maze[x - 1][y - 1];
+        cin >> x >> y >> cost;
+        Edge edge1;
+        edge1.dest = y - 1;
+        edge1.weight = cost;
+        Edge edge2;
+        edge1.dest = x - 1;
+        edge1.weight = cost;
+        maze[x - 1].push_back(edge1);
+        maze[y - 1].push_back(edge2);
     }
 }
 
 void solve() {
     priority_queue<CostDest, vector<CostDest >, greater<CostDest>> que;
+    for (int i = 0; i <= N; i++) {
+        dist1[i] = INF;
+        dist2[i] = INF;
+    }
     que.push(CostDest(0, 0));
     dist1[0] = 0;
 //    dist2[0] = 0;
@@ -51,15 +55,16 @@ void solve() {
         if (dist2[dest] < cost) {//剪枝，如果到2的花费，比之前次短的还多，那就不用了
             continue;
         }
-        for (int i = 0; i < N; i++) {// foreach node
-            int curMinDist = cost + maze[dest][i];
-            if (dist1[i] > curMinDist) {
-                swap(dist1[i], curMinDist); // 记录最近距离，将次近距离放在curMinDist里面
-                que.push(CostDest(dist1[i], i));
+        for (int i = 0; i < maze[dest].size(); i++) {
+            int newDest = maze[dest][i].dest;
+            int curMinDist = cost + maze[dest][i].weight;
+            if (dist1[newDest] > curMinDist) {
+                swap(dist1[newDest], curMinDist); // 记录最近距离，将次近距离放在curMinDist里面
+                que.push(CostDest(dist1[newDest], newDest));
             }
-            if (dist2[i] > curMinDist) {
-                dist2[i] = curMinDist;
-                que.push(CostDest(dist2[i], i));
+            if (dist2[newDest] > curMinDist) {
+                dist2[newDest] = curMinDist;
+                que.push(CostDest(dist2[newDest], newDest));
             }
         }
     }
@@ -68,8 +73,22 @@ void solve() {
 
 
 int main() {
-    readData();
-    solve();
+    while (~scanf("%d%d", &N, &R)) {
+        for (int i = 0; i <= N; i++)
+            maze[i].clear();
+        int u, v, cost;
+        Edge tmp;
+        for (int i = 0; i < R; i++) {
+            scanf("%d%d%d", &u, &v, &cost);
+            tmp.dest = v-1;
+            tmp.weight = cost;
+            maze[u-1].push_back(tmp);
+            tmp.dest = u-1;
+            maze[v-1].push_back(tmp);
+        }
+        solve();
+    }
+
     return 0;
 }
 
